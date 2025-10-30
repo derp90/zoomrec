564c890b97ed33157d332ff31769b18ab0521987
Dayne Putnam <32005229+derp90@users.noreply.github.com>
2025-10-30 18:36:20 -0500

    Update xfce.sh

--- res/xfce.sh
+++ res/xfce.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-set -e
-if [[ "$DEBUG" == "True" ]]; then
-  set -x
-fi
-# Disable screensaver and power management
-xset -dpms &
-xset s noblank &
-xset s 0 0 &
-xset s off &
-
 export DISPLAY=:1
 
-# Start xfce
-/usr/bin/startxfce4 --replace > "$HOME"/xfce.log &
-sleep 1
-cat "$HOME"/xfce.log
+xset -dpms
+xset s off
+xset s noblank
+
+# XFCE session (not --replace)
+startxfce4 &
