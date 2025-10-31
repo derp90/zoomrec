1c18ef94381df56f6e8876afd1a696070d4f9dc2
Dayne Putnam <32005229+derp90@users.noreply.github.com>
2025-10-30 22:18:39 -0500

    Revert "Update entrypoint.sh"
    This reverts commit c906e378e95c3c370f2735db4c216a948a3bf9bc.

--- res/entrypoint.sh
+++ res/entrypoint.sh
@@ -1,17 +1,51 @@
 #!/bin/bash
 set -e
+[ "$DEBUG" == "True" ] && set -x
 
-# Start X virtual framebuffer
-Xvfb :99 -screen 0 1920x1080x24 &
-sleep 2
+cleanup () {
+    kill -s SIGTERM $!
+    exit 0
+}
+trap cleanup SIGINT SIGTERM
 
-# Start PulseAudio
-pulseaudio --start --log-level=error
+mkdir -p "$HOME/.vnc"
+PASSWD_PATH="$HOME/.vnc/passwd"
+echo "$VNC_PW" | vncpasswd -f > "$PASSWD_PATH"
+chmod 600 "$PASSWD_PATH"
 
-# Start D-Bus session
+# Kill leftover X locks
+vncserver -kill "$DISPLAY" &>/dev/null || true
+rm -rf /tmp/.X*-lock /tmp/.X11-unix
+
+echo "Starting dbus session..."
 eval $(dbus-launch --sh-syntax)
 export DBUS_SESSION_BUS_ADDRESS
 export DBUS_SESSION_BUS_PID
 
-echo "Starting Zoom automation..."
-python3 /home/zoomrec/zoomrec.py
+export SESSION_MANAGER="local/$(hostname)/default"
+export QT_X11_NO_MITSHM=1
+
+echo "Starting VNC server..."
+vncserver "$DISPLAY" -depth "$VNC_COL_DEPTH" -geometry "$VNC_RESOLUTION"
+
+echo "Starting XFCE..."
+/usr/bin/startxfce4 > "$HOME/xfce.log" 2>&1 &
+
+sleep 4
+echo "Starting PulseAudio..."
+pulseaudio -D --exit-idle-time=-1 --log-level=error
+
+# Virtual mic setup
+pactl load-module module-null-sink sink_name=speaker >/dev/null
+pactl set-source-volume 1 100%
+pactl load-module module-null-sink sink_name=microphone >/dev/null
+pactl set-source-volume 2 100%
+pactl load-module module-loopback latency_msec=1 source=2 sink=microphone >/dev/null
+pactl load-module module-remap-source master=microphone.monitor source_name=microphone >/dev/null
+pactl set-source-volume 3 60%
+
+echo "Launching Zoom recorder..."
+sleep 5
+python3 -u $HOME/zoomrec.py | tee /tmp/zoomrec.log &
+
+tail -F /tmp/zoomrec.log
