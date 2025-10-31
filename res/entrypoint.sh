8b377f414cfea3c59030b8a997995b3f849f0895
Dayne Putnam <32005229+derp90@users.noreply.github.com>
2025-10-30 22:18:57 -0500

    Revert "Update entrypoint.sh"
    This reverts commit 9da9081dbd4fb92c86156dfd5690c0b460c40e73.

--- res/entrypoint.sh
+++ res/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
-[ "$DEBUG" == "True" ] && set -x
+
+if [[ "$DEBUG" == "True" ]]; then
+  set -x
+fi
 
 cleanup () {
     kill -s SIGTERM $!
@@ -8,44 +11,71 @@ cleanup () {
 }
 trap cleanup SIGINT SIGTERM
 
+VNC_IP=$(hostname -i)
+
+# Change vnc password
 mkdir -p "$HOME/.vnc"
 PASSWD_PATH="$HOME/.vnc/passwd"
-echo "$VNC_PW" | vncpasswd -f > "$PASSWD_PATH"
+
+if [[ -f $PASSWD_PATH ]]; then
+    rm -f "$PASSWD_PATH"
+fi
+
+echo "$VNC_PW" | vncpasswd -f >> "$PASSWD_PATH"
 chmod 600 "$PASSWD_PATH"
 
-# Kill leftover X locks
-vncserver -kill "$DISPLAY" &>/dev/null || true
-rm -rf /tmp/.X*-lock /tmp/.X11-unix
+# Start system bus
+if [ ! -e /var/run/dbus/pid ]; then
+    echo "Starting dbus-daemon..."
+    dbus-daemon --system --fork
+fi
 
-echo "Starting dbus session..."
-eval $(dbus-launch --sh-syntax)
-export DBUS_SESSION_BUS_ADDRESS
-export DBUS_SESSION_BUS_PID
-
+# Export session bus
+export $(dbus-launch)
 export SESSION_MANAGER="local/$(hostname)/default"
-export QT_X11_NO_MITSHM=1
 
-echo "Starting VNC server..."
-vncserver "$DISPLAY" -depth "$VNC_COL_DEPTH" -geometry "$VNC_RESOLUTION"
+# Remove old vnc locks
+vncserver -kill "$DISPLAY" &> "$START_DIR"/vnc_startup.log || rm -rf /tmp/.X*-lock /tmp/.X11-unix &> "$START_DIR"/vnc_startup.log
 
-echo "Starting XFCE..."
-/usr/bin/startxfce4 > "$HOME/xfce.log" 2>&1 &
+echo -e "\nDISPLAY = $DISPLAY\nVNC_COL_DEPTH = $VNC_COL_DEPTH\nVNC_RESOLUTION = $VNC_RESOLUTION\nVNC_IP = $VNC_IP\nVNC_PORT = $VNC_PORT"
+vncserver "$DISPLAY" -depth "$VNC_COL_DEPTH" -geometry "$VNC_RESOLUTION" &> "$START_DIR"/vnc_startup.log
 
-sleep 4
-echo "Starting PulseAudio..."
+echo -e "\nConnect to $VNC_IP:$VNC_PORT"
+
+# Start xfce4
+"$START_DIR"/xfce.sh &> "$START_DIR"/xfce.log
+
+# Cleanup to ensure pulseaudio is stateless
+rm -rf /var/run/pulse /var/lib/pulse /home/zoomrec/.config/pulse
+
+# Start audio
 pulseaudio -D --exit-idle-time=-1 --log-level=error
 
-# Virtual mic setup
-pactl load-module module-null-sink sink_name=speaker >/dev/null
+# Create speaker Dummy-Output
+pactl load-module module-null-sink sink_name=speaker sink_properties=device.description="speaker" > /dev/null
 pactl set-source-volume 1 100%
-pactl load-module module-null-sink sink_name=microphone >/dev/null
+
+# Create microphone Dummy-Output
+pactl load-module module-null-sink sink_name=microphone sink_properties=device.description="microphone" > /dev/null
 pactl set-source-volume 2 100%
-pactl load-module module-loopback latency_msec=1 source=2 sink=microphone >/dev/null
-pactl load-module module-remap-source master=microphone.monitor source_name=microphone >/dev/null
+
+# Map microphone-Output to microphone-Input
+pactl load-module module-loopback latency_msec=1 source=2 sink=microphone > /dev/null
+pactl load-module module-remap-source master=microphone.monitor source_name=microphone source_properties=device.description="microphone" > /dev/null
+# Set microphone Volume
 pactl set-source-volume 3 60%
 
-echo "Launching Zoom recorder..."
+echo -e "\nStart script.."
 sleep 5
-python3 -u $HOME/zoomrec.py | tee /tmp/zoomrec.log &
 
+# Start python script in separated terminal
+if [[ "$DEBUG" == "True" ]]; then
+  # Wait if something failed
+  xfce4-terminal -H --geometry 85x7+0 --title=zoomrec --hide-toolbar --hide-menubar --hide-scrollbar --hide-borders --zoom=-3 -e "bash -c \"python3 -u ${HOME}/zoomrec.py |& tee /tmp/zoomrec.log\"" &
+else
+  # Exit container if something failed
+    xfce4-terminal --geometry 85x7+0 --title=zoomrec --hide-toolbar --hide-menubar --hide-scrollbar --hide-borders --zoom=-3 -e "bash -c \"python3 -u ${HOME}/zoomrec.py |& tee /tmp/zoomrec.log\"" &
+fi
+
+sleep 1
 tail -F /tmp/zoomrec.log
