7fb0bef15ab4cbafc55a17ced9870b616aa2d766
Dayne Putnam <32005229+derp90@users.noreply.github.com>
2025-11-10 20:38:19 -0600

    Update zoomrec.py

--- zoomrec.py
+++ zoomrec.py
@@ -353,6 +353,7 @@ def join(meet_id, meet_pw, duration, description):
     logging.info("Zoom started!")
     start_date = datetime.now()
 
+    time.sleep(3)
     
     # Enter meeting credentials and join
     if not join_by_url:
