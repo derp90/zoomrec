daaecb4f5a2ed701c9f8a2fcf8ff21e6561bd27f
Dayne Putnam <32005229+derp90@users.noreply.github.com>
2025-11-11 18:13:00 -0600

    30s delay after starting zoom for stability reasons

--- zoomrec.py
+++ zoomrec.py
@@ -353,7 +353,7 @@ def join(meet_id, meet_pw, duration, description):
     logging.info("Zoom started!")
     start_date = datetime.now()
 
-    time.sleep(3)
+    time.sleep(30)
     
     # Enter meeting credentials and join
     logging.info("before name")
